5d88a5730e919bc87d13fbe74c65ca4a15b4d34e
Alex Manroe <lanxic@gmail.com>
2022-04-02 22:48:56 +0800

    change background color

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,25 +9,14 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: getProportionateScreenWidth(150),
-                height: getProportionateScreenHeight(150),
-                child: Image.asset("assets/images/LogoLight.png"),
-              ),
-              Form(
-                child: Text("form login"),
-              ),
-              Text("login button"),
-              Spacer(),
-              Text("copyleft 2022"),
-            ],
-          ),
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'HomeView is working',
+          style: TextStyle(fontSize: 20),
         ),
       ),
     );
