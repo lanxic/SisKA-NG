6228e76c1aae7bb4a1585e004f70dbcfa6d05417
Alex Manroe <lanxic@gmail.com>
2022-05-23 17:34:29 +0800

    refactore splash sc

--- lib/app/global/utils/splash_screen.dart
+++ lib/app/global/utils/splash_screen.dart
@@ -6,7 +6,6 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // backgroundColor: kPrimaryColor,
         body: Center(
           child: Container(
             width: getProportionateScreenWidth(150),
